809c039ace6cf67f4ab496a57cdbd34e17efff2a
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2023-11-26 14:15:29 +1300

    docs: remove copyright from source files

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-/*
- * RustyCraft - A voxel engine written in Rust
- * Copyright (C) 2023 SunnyMonster
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 use bevy_ecs::prelude::*;
 
 #[derive(Component)]
@@ -33,9 +15,9 @@ fn print_name(mut query: Query<&mut Name>) {
 fn main() {
     let mut world = World::new();
 
-    world.spawn(
-        Name { name: "Hello World".into() }
-    );
+    world.spawn(Name {
+        name: "Hello World".into(),
+    });
 
     let mut schedule = Schedule::default();
 
