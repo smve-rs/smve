ae34768ef9885d8907651d595f142911707179ae
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2023-12-21 14:38:58 +1300

    style: run rustfmt

--- src/core/window/mod.rs
+++ src/core/window/mod.rs
@@ -16,17 +16,16 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-
 mod resources;
 
 mod icon;
 
+use crate::core::window::resources::Window;
 use bevy_app::{App, Plugin};
 use log::info;
 use winit::event::{Event, WindowEvent};
-use winit::event_loop::{EventLoop};
+use winit::event_loop::EventLoop;
 use winit::window::{Icon, WindowBuilder};
-use crate::core::window::resources::Window;
 
 pub struct WindowPlugin;
 
@@ -41,30 +40,40 @@ fn runner(mut app: App) {
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new()
         .with_title("RustyCraft")
-        .with_window_icon(Some(Icon::from_rgba(icon::IMAGE_DATA.to_vec(), icon::IMAGE_WIDTH as u32, icon::IMAGE_HEIGHT as u32).expect("Bad Icon")))
-        .build(&event_loop).unwrap();
+        .with_window_icon(Some(
+            Icon::from_rgba(
+                icon::IMAGE_DATA.to_vec(),
+                icon::IMAGE_WIDTH as u32,
+                icon::IMAGE_HEIGHT as u32,
+            )
+            .expect("Bad Icon"),
+        ))
+        .build(&event_loop)
+        .unwrap();
 
     app.insert_resource(Window(window));
 
     info!("Entered event loop");
 
     let mut should_update = true;
-    event_loop.run(move |event, window_target| {
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                info!("Window closed, exiting...");
-                should_update = false;
-                window_target.exit();
-            },
-            Event::AboutToWait => {
-                if should_update {
-                    app.update();
+    event_loop
+        .run(move |event, window_target| {
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    info!("Window closed, exiting...");
+                    should_update = false;
+                    window_target.exit();
                 }
-            },
-            _ => {}
-        };
-    }).expect("Event Loop Error");
+                Event::AboutToWait => {
+                    if should_update {
+                        app.update();
+                    }
+                }
+                _ => {}
+            };
+        })
+        .expect("Event Loop Error");
 }
