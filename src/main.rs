68510cae936738fd333f92b89da086af2c52070d
ItsRainyMonster <113001652+ItsRainyMonster@users.noreply.github.com>
2024-01-05 14:53:20 +1300

    style: mess up formatting

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ use crate::core::window::WindowPlugin;
 use bevy_app::prelude::*;
 use env_logger::Env;
 
-fn main() {
+fn main() 
+    
+    
+    {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     App::new().add_plugins(WindowPlugin::default()).run();
 }
+
+
+
