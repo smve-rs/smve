c912516329e6abb4504a31cd8f2170b6ccfa86ab
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2024-03-19 17:35:57 +1300

    fix: remove surface resizing

--- src/core/graphics/mod.rs
+++ src/core/graphics/mod.rs
@@ -1,5 +1,5 @@
 use crate::core::graphics::resources::GraphicsState;
-use crate::core::graphics::systems::{u_create_surface, u_destroy_surface, u_resize};
+use crate::core::graphics::systems::{u_create_surface, u_destroy_surface};
 use bevy_app::{App, Plugin, Update};
 
 mod adapter_selection_utils;
@@ -14,7 +14,6 @@ impl Plugin for GraphicsPlugin {
         // By implementing ready() to check if the async process is done.
         app.insert_non_send_resource(pollster::block_on(GraphicsState::new()));
         app.add_systems(Update, u_create_surface);
-        app.add_systems(Update, u_resize);
         app.add_systems(Update, u_destroy_surface);
     }
 }
