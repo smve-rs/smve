f5d0aba952b9321b4ef5b4b5f61a5f120af0e1d0
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2024-04-06 20:38:20 +1300

    feat: add file logging for dist build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(feature = "windowed", windows_subsystem = "windows")]
 #![deny(missing_docs)]
 #![doc(html_favicon_url = "https://cdn.jsdelivr.net/gh/ItsSunnyMonster/ruxel/images/icon.png")]
 #![doc(html_logo_url = "https://cdn.jsdelivr.net/gh/ItsSunnyMonster/ruxel/images/icon.png")]
@@ -14,15 +15,97 @@ pub mod core;
 
 use crate::core::window::WindowPlugin;
 use bevy_app::prelude::*;
-use env_logger::Env;
+use cfg_if::cfg_if;
 
 /// The main entry point for the application.
 ///
 /// Initializes the logger and runs the [bevy application](https://docs.rs/bevy_app/latest/bevy_app/).
 pub fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    init_logger();
 
     log_panics::init();
 
     App::new().add_plugins(WindowPlugin::default()).run();
+}
+
+/// Initializes loggers based on the features enabled.
+///
+/// If the `log-to-file` feature is enabled, logs (level DEBUG) will be written to a file.
+/// Otherwise, logs (level INFO) will be written to the console with `env_logger`.
+/// 
+/// # Panics
+/// - If the log directory cannot be initialized
+/// - If the logger cannot be initialized
+/// - If the log file cannot be created
+fn init_logger() {
+    cfg_if! {
+        if #[cfg(feature="log-to-file")] {
+            let date = chrono::Utc::now();
+            let log_file = date.format("logs/ruxel_log_%Y-%m-%d_%H-%M-%S-%f.log").to_string();
+            
+            initialize_log_directory().unwrap_or_else(|e| {
+                panic!("Failed to initialize log directory: {}", e);
+            });
+
+            fern::Dispatch::new()
+                .format(|out, message, record| {
+                    out.finish(format_args!(
+                        "[{} {} {}] {}",
+                        humantime::format_rfc3339_seconds(std::time::SystemTime::now()),
+                        record.level(),
+                        record.target(),
+                        message
+                    ))
+                })
+                .level(log::LevelFilter::Debug)
+                .chain(fern::log_file(log_file).unwrap_or_else(|e| {
+                    panic!("Failed to create log file: {}", e);
+                }))
+                .apply().unwrap_or_else(|e| {
+                    panic!("Failed to initialize logger: {}", e);
+                });
+        } else {
+            env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+        }
+    }
+}
+
+/// Initializes the log directory and compresses old logs.
+#[cfg(feature = "log-to-file")]
+fn initialize_log_directory() -> Result<(), std::io::Error> {
+    // Create the logs directory if it doesn't exist
+    if !std::path::Path::new("logs").exists() {
+        std::fs::create_dir("logs")?;
+    }
+
+    // Compress old logs
+    for log in std::fs::read_dir("logs")? {
+        let log = log?;
+        let path = log.path();
+        let metadata = log.metadata()?;
+
+        if metadata.is_file() {
+            let file_name = path
+                .file_name()
+                .expect("Path should be a file")
+                .to_str()
+                .expect("Path should contain valid unicode");
+            if file_name.ends_with(".log") {
+                let compressed_file_name = format!("{}.gz", file_name);
+                let compressed_file_path = path.with_file_name(compressed_file_name);
+
+                let file = std::fs::File::open(&path)?;
+                let mut reader = std::io::BufReader::new(file);
+                let mut compressed_file = flate2::write::GzEncoder::new(
+                    std::fs::File::create(&compressed_file_path)?,
+                    flate2::Compression::default(),
+                );
+
+                std::io::copy(&mut reader, &mut compressed_file)?;
+                std::fs::remove_file(&path)?;
+            }
+        }
+    }
+
+    Ok(())
 }
