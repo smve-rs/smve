038754423eae48adcd6f3acdb43ffdc6835194a8
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2024-07-29 20:45:59 +1200

    feat: add basic triangle renderer

--- crates/smve_render/src/lib.rs
+++ crates/smve_render/src/lib.rs
@@ -1,3 +1,39 @@
-pub fn test_print(s: &str) {
-    println!("{s}");
+pub mod components;
+mod render;
+
+use crate::components::Triangle;
+use crate::render::draw::{
+    prepare_triangle_phase_item_buffers, queue_triangle_phase_item, DrawTriangleCommands,
+    TrianglePipeline, WithTriangle,
+};
+use bevy_app::{App, Plugin, PostUpdate};
+use bevy_core_pipeline::core_3d::Opaque3d;
+use bevy_ecs::schedule::IntoSystemConfigs;
+use bevy_render::extract_component::ExtractComponentPlugin;
+use bevy_render::render_phase::AddRenderCommand;
+use bevy_render::render_resource::SpecializedRenderPipelines;
+use bevy_render::view::VisibilitySystems;
+use bevy_render::{view, Render, RenderApp, RenderSet};
+
+pub struct RenderPlugin;
+
+impl Plugin for RenderPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(ExtractComponentPlugin::<Triangle>::default())
+            .add_systems(
+                PostUpdate,
+                view::check_visibility::<WithTriangle>.in_set(VisibilitySystems::CheckVisibility),
+            );
+
+        app.get_sub_app_mut(RenderApp)
+            .unwrap()
+            .init_resource::<TrianglePipeline>()
+            .init_resource::<SpecializedRenderPipelines<TrianglePipeline>>()
+            .add_render_command::<Opaque3d, DrawTriangleCommands>()
+            .add_systems(
+                Render,
+                prepare_triangle_phase_item_buffers.in_set(RenderSet::Prepare),
+            )
+            .add_systems(Render, queue_triangle_phase_item.in_set(RenderSet::Queue));
+    }
 }
--- examples/triangle.rs
+++ examples/triangle.rs
@@ -1,5 +1,41 @@
-use smve::render::test_print;
+use bevy::math::Vec3A;
+use bevy::prelude::*;
+use bevy::render::primitives::Aabb;
+use bevy::window::WindowResolution;
+use smve::render::components::Triangle;
+use smve::render::RenderPlugin;
 
 fn main() {
-    test_print("Hello World!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "SMve".into(),
+                resolution: WindowResolution::new(800.0, 600.0),
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::srgb(0.0, 0.6328125, 0.92578125)))
+        .add_plugins(RenderPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands
+        .spawn(SpatialBundle {
+            visibility: Visibility::Visible,
+            transform: Transform::IDENTITY,
+            ..default()
+        })
+        .insert(Aabb {
+            center: Vec3A::ZERO,
+            half_extents: Vec3A::splat(0.5),
+        })
+        .insert(Triangle);
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
