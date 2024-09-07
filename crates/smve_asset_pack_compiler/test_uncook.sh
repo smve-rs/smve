d51270f7e179e5b8af7add4494cdbe34a6a82d62
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2024-09-07 17:04:01 +1200

    revert: removed asset_pack_compiler into its own repo

--- crates/smve_asset_pack_compiler/test_uncook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-mkdir output
-cargo run -p smve_asset_pack_compiler -- -a test_assets -o output/out.smap -u example_uncookers/e_uncooker.lua --no-default-uncookers
