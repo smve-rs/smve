82867e18bf23722ed3cfd372b8ba332d8cc16698
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2024-12-15 12:45:21 +1300

    style(asset_pack): fix typo

--- crates/smve_asset_pack/src/pack_io/common/mod.rs
+++ crates/smve_asset_pack/src/pack_io/common/mod.rs
@@ -8,7 +8,7 @@ bitflags! {
     /// See [File Flags](https://github.com/smve-rs/asset_pack/blob/master/docs/specification/v1.md#file-flags)
     #[derive(Copy, Clone, Debug)]
     pub struct Flags: u8 {
-        /// If the asset is stored in its processed and optimised form.
+        /// If the asset is stored in its processed and optimized form.
         const PROCESSED = 1 << 0;
         /// If the asset is marked as pack-unique.
         const UNIQUE = 1 << 1;
